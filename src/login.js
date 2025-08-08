b11006291ff366ed2ba9b113f3cea2d575fc6194
Simone Cerino <cerino.2022890@studenti.uniroma1.it>
2025-08-08 20:44:37 +0200

    Added login

---
+++ src/login.js
@@ -0,0 +1,10 @@
+function togglePassword() {
+  const passwordInput = document.getElementById("password");
+  const icon = document.getElementById("togglePasswordIcon");
+
+  const isPassword = passwordInput.type === "password";
+  passwordInput.type = isPassword ? "text" : "password";
+
+  icon.classList.toggle("fa-eye");
+  icon.classList.toggle("fa-eye-slash");
+}
